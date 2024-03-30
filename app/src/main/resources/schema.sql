4904baac38645f9e3cb6fc1d2bfd88e58dc3fd44
snswd <sanswed@gmail.com>
2024-03-30 10:08:55 +0300

    step#7

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -2,17 +2,20 @@ DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls(
-        id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE NOT NULL,
         name varchar(255) UNIQUE NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        CONSTRAINT pk_urls PRIMARY KEY (id)
 );
 
 CREATE TABLE url_checks(
-        id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE NOT NULL,
         url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        status_code INT,
+        status_code INTEGER,
         title VARCHAR(255),
         h1 VARCHAR(255),
-        description TEXT
+        description TEXT,
+        FOREIGN KEY (url_id) REFERENCES urls (id),
+        CONSTRAINT pk_url_checks PRIMARY KEY (id)
 );
